4ff299d1fae44a69089be83a171ec57176d1d6cf
Arjun Rajan <arjunrajan991@gmail.com>
2025-07-07 07:34:45 +0530

    4 to 2 space indent for personal reasons

--- db.py
+++ db.py
@@ -3,47 +3,47 @@ import psycopg2
 conn = None
 
 def initialize_db():
-    global conn
-    conn = psycopg2.connect(dbname="postgres",
-                            host="localhost",
-                            user="postgres",
-                            password="123456789",
-                            port=5432)
+  global conn
+  conn = psycopg2.connect(dbname="postgres",
+                          host="localhost",
+                          user="postgres",
+                          password="123456789",
+                          port=5432)
 
-    cur = conn.cursor()
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS urls (
-        id SERIAL PRIMARY KEY,
-        long_url TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-    """)
+  cur = conn.cursor()
+  cur.execute("""
+  CREATE TABLE IF NOT EXISTS urls (
+      id SERIAL PRIMARY KEY,
+      long_url TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+  """)
 
-    conn.commit()
-    cur.close()
+  conn.commit()
+  cur.close()
 
 
 def insert_db(long_url):
-    global conn
-    cur = conn.cursor()
-    cur.execute("""
-        INSERT INTO urls (long_url) VALUES (%s) RETURNING id;
-        """, (long_url,))
-    insert_id = cur.fetchone()[0]
+  global conn
+  cur = conn.cursor()
+  cur.execute("""
+      INSERT INTO urls (long_url) VALUES (%s) RETURNING id;
+      """, (long_url,))
+  insert_id = cur.fetchone()[0]
 
-    conn.commit()
-    cur.close()
-    return insert_id
+  conn.commit()
+  cur.close()
+  return insert_id
 
 def redirect_lookup(serial_id):
-    global conn
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT long_url FROM urls WHERE id = %s;
-        """ , (serial_id,))
-    
-    long_url = cur.fetchone()[0]
-    return long_url
+  global conn
+  cur = conn.cursor()
+  cur.execute("""
+      SELECT long_url FROM urls WHERE id = %s;
+      """ , (serial_id,))
+
+  long_url = cur.fetchone()[0]
+  return long_url
 
 def close_db():
-    conn.close()
+  conn.close()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import AnyHttpUrl, BaseModel, ValidationError, \
-        field_validator
+    field_validator
 import uvicorn
 import db
 import base62
@@ -11,73 +11,73 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 class URLModel(BaseModel):
-    url: AnyHttpUrl
+  url: AnyHttpUrl
 
-    @field_validator("url")
-    @classmethod
-    def custom_validator(cls, v):
-        if "." not in v.host:
-            raise ValueError("Invalid URL.")
-        if v.host.startswith((".","-","_","/")):
-            raise ValueError("Invalid URL.")
-        if v.host == "localhost":
-            raise ValueError("Invalid URL.")
-        return v
+  @field_validator("url")
+  @classmethod
+  def custom_validator(cls, v):
+    if "." not in v.host:
+      raise ValueError("Invalid URL.")
+    if v.host.startswith((".","-","_","/")):
+      raise ValueError("Invalid URL.")
+    if v.host == "localhost":
+      raise ValueError("Invalid URL.")
+    return v
 
 
 def validate_url(url: str):
-    try:
-        valid_url = URLModel(url=url)
-        return valid_url.url
-    except ValidationError as e:
-        raise ValueError("Invalid URL.")
+  try:
+    valid_url = URLModel(url=url)
+    return valid_url.url
+  except ValidationError as e:
+    raise ValueError("Invalid URL.")
 
 
 @app.get("/")
 def index(request: Request):
-    return templates.TemplateResponse("index.html",{
-        "request": request,
-        "input_value": None,
-        "error": None})
+  return templates.TemplateResponse("index.html",{
+    "request": request,
+    "input_value": None,
+    "error": None})
 
 
 @app.post("/shorturl")
 def short_url(request: Request, long_url: str = Form(...)):
-    try:
-        valid_url = validate_url(long_url)
-    except ValueError:
-        return templates.TemplateResponse("index.html",{
-            "request": request,
-            "input_value": long_url,
-            "error": "Invalid URL. Please enter a valid URL."
-            })
-        
-    db_id = db.insert_db(long_url)
+  try:
+    valid_url = validate_url(long_url)
+  except ValueError:
+    return templates.TemplateResponse("index.html",{
+      "request": request,
+      "input_value": long_url,
+      "error": "Invalid URL. Please enter a valid URL."
+      })
 
-    # base62 encoding
-    encoded_string = base62.encode(db_id)
-    short_url = f"http://127.0.0.1:8000/su/{encoded_string}"
+  db_id = db.insert_db(long_url)
 
-    return templates.TemplateResponse("result.html",{
-        "request": request,
-        "original": valid_url,
-        "short": short_url
-        })
+  # base62 encoding
+  encoded_string = base62.encode(db_id)
+  short_url = f"http://127.0.0.1:8000/su/{encoded_string}"
+
+  return templates.TemplateResponse("result.html",{
+    "request": request,
+    "original": valid_url,
+    "short": short_url
+    })
 
 
 @app.get("/su/{short_id}")
 def redirect_to_long(short_id: str):
 
-    # base62 decoding
-    serial_id = base62.decode(short_id)
-    long_url = db.redirect_lookup(serial_id)
+  # base62 decoding
+  serial_id = base62.decode(short_id)
+  long_url = db.redirect_lookup(serial_id)
 
-    if long_url:
-        return RedirectResponse(long_url)
-    else:
-        raise HTTPException(status_code=404)
+  if long_url:
+    return RedirectResponse(long_url)
+  else:
+    raise HTTPException(status_code=404)
 
 if __name__ == "__main__":
-    db.initialize_db()
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-    db.close_db()
+  db.initialize_db()
+  uvicorn.run(app, host="127.0.0.1", port=8000)
+  db.close_db()
