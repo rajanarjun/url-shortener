f9516113ab7644ede5176213a854ca644429ec06
Arjun Rajan <arjunrajan991@gmail.com>
2025-11-08 18:53:06 +0530

    utilities lib

---
+++ utils.py
@@ -0,0 +1,13 @@
+"""
+1. validate_url : return boolean
+2. InvalidURL class : just pass
+"""
+
+
+class InvalidURL(Exception):
+    pass
+
+
+def validate_url(url_string):
+    # return boolean
+    pass
